e20bcc22f4502762ecca5ddde45c022747cb1eab
Jiwon Moon <mon99745@dreamsecurity.com>
2025-09-15 13:54:33 +0900

    build. [공통] JWT 라이브러리 버전 업데이트

--- license-common/src/main/java/com/licensecommon/config/SpringDocConfig.java
+++ license-common/src/main/java/com/licensecommon/config/SpringDocConfig.java
@@ -6,15 +6,15 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-@Configuration
 @ConfigurationProperties(prefix = "common.api-doc")
+@EnableConfigurationProperties(SpringDocConfig.class)
 public class SpringDocConfig {
 
 	private String title;
