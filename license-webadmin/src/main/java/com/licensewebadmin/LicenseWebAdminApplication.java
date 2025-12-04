bafac6b68047381f25bcb1aab845575eeaf88cc0
Jiwon Moon <mon99745@dreamsecurity.com>
2025-12-04 14:48:11 +0900

    chore: Modifying the Naming

--- license-webadmin/src/main/java/com/licensewebadmin/LicenseWebAdminApplication.java
+++ license-webadmin/src/main/java/com/licensewebadmin/LicenseWebAdminApplication.java
@@ -4,17 +4,16 @@ import com.licensecommon.config.SpringDocProperties;
 import io.jwt4j.lite.core.config.TokenAutoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @EnableConfigurationProperties(SpringDocProperties.class)
 @SpringBootApplication(scanBasePackages = {"com.licensewebadmin", "com.licensevalidator", "com.licensecommon"})
 @Import(TokenAutoConfig.class)
-public class LicenseWebadminApplication {
+public class LicenseWebAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LicenseWebadminApplication.class, args);
+		SpringApplication.run(LicenseWebAdminApplication.class, args);
 	}
 
 }
